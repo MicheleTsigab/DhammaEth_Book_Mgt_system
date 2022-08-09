8f78dc6535faba9c3b76d6731fe7616bf92806dd
Michele <micheletsigab@gmail.com>
2022-08-09 22:09:07 +0300

    models instance, borrowed book

--- DhammaEth_Book_Mgt/admin.py
+++ DhammaEth_Book_Mgt/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author,  Book, BorrowedBook,  Instance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Instance)
+admin.site.register(BorrowedBook)
--- DhammaEth_Book_Mgt/models.py
+++ DhammaEth_Book_Mgt/models.py
@@ -1,24 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=200)
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 class Book(models.Model):
     title=models.CharField(max_length=100)
-    author=models.CharField(max_length=300)
+    author=models.ManyToManyField('Author', null=True)
     cover=models.ImageField(blank=True, null=True)
-    copies=models.IntegerField()
-
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 class Admin(models.Model):
     pass
 class Member(models.Model):
     dhamma_id=models.CharField(max_length=40,null=True)
     first_name=models.CharField(max_length=40)
     last_name=models.CharField(max_length=40)
-    phone=models.CharField(max_length=40) 
-    email=models.EmailField()
-    borrowed_books=models.ManyToManyField(Book,through='Borrowed_Book')
-class Borrowed_Book(models.Model):
+    phone=models.CharField(max_length=40,null=True) 
+    email=models.EmailField(null=True)
+class Instance(models.Model):
+    """A specific copy of a Book that is actually borrow-able
+    """
+    id = models.BigAutoField(primary_key=True)
     book=models.ForeignKey(Book,on_delete=models.CASCADE)
-    member=models.ForeignKey(Member,on_delete=models.CASCADE)
-    borrowed_date=models.DateField(auto_now=True)
-    return_date=models.DateField()
+    status=models.ForeignKey('BorrowedBook',on_delete=models.SET_NULL,null=True)
+class BorrowedBook(models.Model):
+    #Book=models.ForeignKey(Instance,on_delete=models.CASCADE)
+    borrower = models.ForeignKey(Member, on_delete=models.CASCADE)
+    borrowed_date=models.DateField(auto_now=True,null=True)
+    return_date=models.DateField(null=True)
     
