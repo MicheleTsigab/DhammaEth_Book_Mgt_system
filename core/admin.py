173dfbd717e27cd6aa5af0b8a034259e3919f959
Michele <micheletsigab@gmail.com>
2022-08-10 19:47:10 +0300

    Listview book

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Author,  Book, BorrowedBook,  Instance
+from .models import Author,  Book, BorrowedBook,  Instance, Language, Member
 
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Instance)
 admin.site.register(BorrowedBook)
+admin.site.register(Language)
+admin.site.register(Member)
--- core/models.py
+++ core/models.py
@@ -9,21 +9,21 @@ class Book(models.Model):
     title=models.CharField(max_length=100)
     author=models.ForeignKey('Author',on_delete=models.SET_NULL, null=True)
     cover=models.ImageField(blank=True, null=True)
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True,blank=True)
 class Admin(models.Model):
     pass
 class Member(models.Model):
-    dhamma_id=models.CharField(max_length=40,null=True)
+    dhamma_id=models.CharField(max_length=40,null=True,blank=True)
     first_name=models.CharField(max_length=40)
     last_name=models.CharField(max_length=40)
-    phone=models.CharField(max_length=40,null=True) 
-    email=models.EmailField(null=True)
+    phone=models.CharField(max_length=40,null=True,blank=True) 
+    email=models.EmailField(null=True,blank=True)
 class Instance(models.Model):
     """A specific copy of a Book that is actually borrow-able
     """
     id = models.BigAutoField(primary_key=True)
     book=models.ForeignKey(Book,on_delete=models.CASCADE)
-    status=models.ForeignKey('BorrowedBook',on_delete=models.SET_NULL,null=True)
+    status=models.ForeignKey('BorrowedBook',on_delete=models.SET_NULL,null=True,blank=True)
 class BorrowedBook(models.Model):
     #Book=models.ForeignKey(Instance,on_delete=models.CASCADE)
     borrower = models.ForeignKey(Member, on_delete=models.CASCADE)
