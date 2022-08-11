fdfbbdd609f6cee97f52a2d9140dd9fcbcd9c8d0
Michele <micheletsigab@gmail.com>
2022-08-11 18:54:35 +0300

    user authentication

--- DhammaEth_Book_Mgt_system/settings.py
+++ DhammaEth_Book_Mgt_system/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig'
+    'core.apps.CoreConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -117,9 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+LOGIN_REDIRECT_URL = '/'
 STATIC_URL = 'static/'
-
+CRISPY_TEMPLATE_PACK='bootstrap4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
