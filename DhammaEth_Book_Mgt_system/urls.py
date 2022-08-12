cd1d5e91ee32f14ff15b5fa0e9be4d563ead43b7
Michele <micheletsigab@gmail.com>
2022-08-12 21:16:06 +0300

    remove searchable

--- DhammaEth_Book_Mgt_system/urls.py
+++ DhammaEth_Book_Mgt_system/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,re_path
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    re_path('^searchableselect/', include('searchableselect.urls')),
-    path('',include('core.urls')),
+    
+    path('library/',include('core.urls')),
     
 ]
 if settings.DEBUG:
