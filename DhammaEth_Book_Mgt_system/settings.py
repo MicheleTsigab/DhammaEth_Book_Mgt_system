8830edd7a25f14a4c9112d85bd49799d83584128
Michele <micheletsigab@gmail.com>
2022-08-12 21:15:51 +0300

    removed searchable and add dal

--- DhammaEth_Book_Mgt_system/settings.py
+++ DhammaEth_Book_Mgt_system/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'dal',
+    'dal_select2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'crispy_forms',
-    'searchableselect',
+    # 'searchableselect',
 ]
 
 MIDDLEWARE = [
