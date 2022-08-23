8dc8d81d0135a5ae27253a90f0b46479fa466049
Michele <micheletsigab@gmail.com>
2022-08-23 22:20:18 +0300

    return func based view

--- core/views.py
+++ core/views.py
@@ -1,14 +1,13 @@
 from django.views import generic
-from django.shortcuts import render
-from .filters import InstanceFilter
 
-from core.forms import AddBookForm,AddAuthorForm,AddMemberForm, GetInstance, LendBookForm,ReturnBookForm
+from django.shortcuts import render,get_object_or_404
+from .filters import InstanceFilter,AvailableBooksFilter
+from core.forms import *
 from django.urls import reverse_lazy
-from .models import Book, Author, Instance, Member
+from .models import Book, Author, Instance, Language, Member
 from django_filters.views import FilterView
 def index(request):
     """View function for home page of site."""
-
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = Instance.objects.all().count()
@@ -21,6 +20,7 @@ def index(request):
         'num_authors': num_authors,
     }
     return render(request, 'index.html', context=context)
+
 class BookListView(generic.ListView):
     model=Book
     paginate_by = 10
@@ -28,55 +28,74 @@ class BookDetailView(generic.DetailView):
     model=Book
 class InstanceListView(generic.ListView):
     model=Instance
-class InstanceFilterView(FilterView):
-    model = Instance    
-    template_name = 'core/instance_list.html' 
-    filterset_class = InstanceFilter
+
 class InstanceDetailView(generic.DetailView):
     model=Instance
 class AuthorListView(generic.ListView):
     model=Author
 class AuthorDetailView(generic.DetailView):
     model=Author
+### Creation Views
+class AddInstanceView(generic.FormView):
+    model=Instance
+    form_class=MultipleInstance
+    template_name='core/add_model_partial.html'
+
+
 class AddBookView(generic.CreateView):
     model=Book
     form_class = AddBookForm
     template_name = 'core/add_book.html'
-    success_url = reverse_lazy('index')
+    def form_valid(self, form):
+        num = form.cleaned_data['copies']
+        res=super().form_valid(form)
+        book=form.instance
+        if num:
+            num=int(num)
+        for _ in range(num):
+            Instance(book=book).save()
+        return res
 class AddAuthorView(generic.CreateView):
     model=Author
     form_class = AddAuthorForm
-    template_name='core/add_author.html'
+    template_name='core/add_model_partial.html'
     success_url=reverse_lazy('index')
-class LendBookView(generic.CreateView):
-    model=Instance
-    form_class=LendBookForm
-    template_name='core/lend_book.html'
-    success_url=reverse_lazy('index')
+
 class AddMemberView(generic.CreateView):
     model=Member
     form_class=AddMemberForm
     template_name='core/add_member.html'
     success_url=reverse_lazy('index')
-class ReturnBookView(generic.FormView):
-    
-    #Should change this view to update view if lending history is a functional requirement
-    model=Instance
-    form_class=GetInstance
-    template_name='core/get_instance.html'
+class AddLanguageView(generic.CreateView):
+    model=Language
+    form_class=AddLanguageForm
+    template_name='core/add_model_partial.html'
     success_url=reverse_lazy('index')
-from dal import autocomplete
-
-class InstanceAutoComplete(autocomplete.Select2QuerySetView):
-    #template='core/get_instance.html'
-    def get_queryset(self):
-        # Don't forget to filter out results depending on the visitor !
-        if not self.request.user.is_authenticated:
-            return Book.objects.none()
-
-        qs = Book.objects.all()
-
-        if self.q:
-            qs = qs.filter(id__istartswith=self.q)
-
-        return qs
+class InstanceFilterView(FilterView):
+    model = Instance    
+    template_name = 'core/instance_list.html' 
+    filterset_class = InstanceFilter
+class AvailableBookView(FilterView):
+    model=Instance
+    filterset_class=AvailableBooksFilter
+    template_name='core/available_books.html'
+class BorrowedBookView(FilterView):
+    model=Instance
+    filterset_class=InstanceFilter
+    template_name='core/borrowed_books.html'
+    
+class LendInstance(generic.UpdateView):
+    model=Instance
+    form_class=LendBookForm
+    template_name='core/lend_book.html'
+# @loginrequired
+def ReturnInstance(request,pk):
+   instance=get_object_or_404(Instance, pk=pk)
+   instance.borrower=None
+   instance.borrowed_date=None
+   instance.return_date=None
+   context={
+    'instance':instance,
+    'status':'returned'
+   }
+   return render(request, 'core/instance_detail.html', context=context)
