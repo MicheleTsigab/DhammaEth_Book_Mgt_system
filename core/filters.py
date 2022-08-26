a91026a2437bb780b8d273a9b9cca1ca3aa666ce
Michele <micheletsigab@gmail.com>
2022-08-26 18:46:42 +0300

    removed filter func

--- core/filters.py
+++ core/filters.py
@@ -1,22 +1,36 @@
-import django_filters
 
-from .models import  Instance
-def borrow_instance(request):
-    pass
+import django_filters
+from django import forms
+from .models import  Book, Instance
+from .forms import InstanceFilterForm
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class InstanceFilter(django_filters.FilterSet):
+    book__title=django_filters.CharFilter(
+        label="Book Title",
+        lookup_expr='icontains')
+    borrower__first_name=django_filters.CharFilter(
+        label='First Name',
+        lookup_expr='icontains')
+    borrower__last_name=django_filters.CharFilter(
+        label='Last Name',
+        lookup_expr='icontains')
+    borrowed_date=django_filters.DateFilter(widget=DateInput())
     class Meta:
         model=Instance
-        fields={
-            'id':['exact'],
-            'book__title':['contains'],
-            'borrower__last_name':['contains'],
-            'borrower__first_name':['contains'],
-            'borrowed_date':['exact']
-            
-        }
+        
+        fields={'id':['exact']}
 class AvailableBooksFilter(django_filters.FilterSet): 
+    book__title=django_filters.CharFilter(
+        label="Book Title",
+        lookup_expr='icontains',
+        )
     class Meta:
         model = Instance
-        fields = {
-        'book__title':['contains']
-        }
+        fields = ['book__title']
+class BookFilter(django_filters.FilterSet):
+    search=django_filters.CharFilter(field_name='title',lookup_expr='icontains')
+    class Meta:
+        model=Book
+        fields=['search'] 
